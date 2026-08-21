74402222ed30fe782ce77e742e679459be8be91b
Neal Sharma <sharmnten@gmail.com>
2026-08-20 22:28:48 -0500

    More images.

--- js/slideshow-manifest.js
+++ js/slideshow-manifest.js
@@ -14,6 +14,46 @@
     "src": "assets/slideshow/03-zests.jpg",
     "title": "Zests",
     "alt": "Youth sailing: Zests"
+  },
+  {
+    "src": "assets/slideshow/IMG59930334353346700.jpg",
+    "title": "IMG59930334353346700",
+    "alt": "Youth sailing: IMG59930334353346700"
+  },
+  {
+    "src": "assets/slideshow/IMG3741786270949018312.jpg",
+    "title": "IMG3741786270949018312",
+    "alt": "Youth sailing: IMG3741786270949018312"
+  },
+  {
+    "src": "assets/slideshow/IMG4072112768973319724.jpg",
+    "title": "IMG4072112768973319724",
+    "alt": "Youth sailing: IMG4072112768973319724"
+  },
+  {
+    "src": "assets/slideshow/IMG4266541135499900558.jpg",
+    "title": "IMG4266541135499900558",
+    "alt": "Youth sailing: IMG4266541135499900558"
+  },
+  {
+    "src": "assets/slideshow/IMG4780312825659484616.jpg",
+    "title": "IMG4780312825659484616",
+    "alt": "Youth sailing: IMG4780312825659484616"
+  },
+  {
+    "src": "assets/slideshow/IMG5022214307597748378.jpg",
+    "title": "IMG5022214307597748378",
+    "alt": "Youth sailing: IMG5022214307597748378"
+  },
+  {
+    "src": "assets/slideshow/IMG6569237602442246047.jpg",
+    "title": "IMG6569237602442246047",
+    "alt": "Youth sailing: IMG6569237602442246047"
+  },
+  {
+    "src": "assets/slideshow/IMG6890960302933409169.jpg",
+    "title": "IMG6890960302933409169",
+    "alt": "Youth sailing: IMG6890960302933409169"
   }
 ];
 })();
